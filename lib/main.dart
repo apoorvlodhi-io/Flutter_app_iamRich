53f04ac75e287e95e536c8cb094d1510d4c8efb9
apoorvlodhi-io <apoorvlodhi21@gmail.com>
2020-01-06 18:41:21 +0530

    first flutter app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('I AM RICH'),
+            backgroundColor: Colors.blueGrey[800],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          )),
+    ),
+  );
+}
